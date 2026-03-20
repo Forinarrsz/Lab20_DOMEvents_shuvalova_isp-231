68c9fde92826625f7978b607144383b85f0245da
dazzes4545 <dazzes7619@gmail.com>
2026-03-20 14:06:39 +0300

    Step 5: click event handling

--- main.js
+++ main.js
@@ -11,3 +11,11 @@ style.style.padding = "10px";
 text.style.fontSize = "18px";
 text.style.fontStyle = "italic";
 text.style.color = "darkgreen";
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    alert("button clicked");
+    btn.style.backgroundColor= "#4CAF50";
+    btn.style.color = "white";
+});
