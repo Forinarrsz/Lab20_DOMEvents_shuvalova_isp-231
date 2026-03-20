62bca793a7017fe7bb2b67a0f1413445ddef7de1
dazzes4545 <dazzes7619@gmail.com>
2026-03-20 14:55:16 +0300

    Step 9: form validation in JavaScript

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ console.log(text);
 title.textContent = "DOM изменен через js";
 title.style.color = "red";
 title.style.backgroundColor = "#333";
-style.style.padding = "10px";
+text.style.padding = "10px";
 text.style.fontSize = "18px";
 text.style.fontStyle = "italic";
 text.style.color = "black";
@@ -15,43 +15,57 @@ text.style.color = "black";
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
-    alert("button clicked");
-    btn.style.backgroundColor= "#4CAF50";
-    btn.style.color = "white";
+  alert("button clicked");
+  btn.style.backgroundColor = "#4CAF50";
+  btn.style.color = "white";
 });
 
-const input = document.getElementById("input");
+const input = document.getElementById("nameInput");
 const button = document.getElementById("showName");
 const output = document.getElementById("output");
 const clearButton = document.getElementById("clearButton");
-
 
 const form = document.getElementById("userForm");
 const usernameInput = document.getElementById("username");
 const ageInput = document.getElementById("age");
 const result = document.getElementById("formResult");
 
-
 button.addEventListener("click", () => {
-    if (input.value.trim() !== "") {
-        output.textContent = `hi, ${input.value}!`;
-        output.style.color = "green";
-    } else {
-        output.textContent = "please try again";
-        output.style.color = "red";
-    }
+  if (input.value.trim() !== "") {
+    output.textContent = `hi, ${input.value}!`;
+    output.style.color = "green";
+  } else {
+    output.textContent = "please try again";
+    output.style.color = "red";
+  }
 });
 
-clearButton = addEventListener("click", () => {
-    input.value = "";
-    output.textContent = "";
-    output.style.color = "";
-    input.focus();
+clearButton.addEventListener("click", () => {
+  input.value = "";
+  output.textContent = "";
+  output.style.color = "";
+  input.focus();
 });
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const name = usernameInput.value;
-    const age = ageInput.value;
-    result.textContent = `Name: ${name}, Age: ${age}`;
+  event.preventDefault();
+  const name = usernameInput.value.trim();
+  const age = Number(ageInput.value);
+  result.textContent = `Name: ${name}, Age: ${age}`;
+  if (name === "") {
+    result.textContent = "Try again";
+    result.style.color = "red";
+    usernameInput.focus();
+    return;
+  }
+  if (isNaN(age) || age <= 0 || age > 120) {
+    result.textContent = "try again";
+    result.style.color = "red";
+    ageInput.focus();
+    ageInput.value = "";
+    return;
+  }
+  result.textContent = `saved: ${name}, ${age}`;
+  result.style.color = "green";
+  form.reset();
 });
