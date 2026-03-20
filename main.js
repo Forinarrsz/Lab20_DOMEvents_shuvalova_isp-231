64f55dc72ad9d2f086df826214d3716db7857ce0
dazzes4545 <dazzes7619@gmail.com>
2026-03-20 14:18:48 +0300

    Step 6: input and button interaction

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ title.style.backgroundColor = "#333";
 style.style.padding = "10px";
 text.style.fontSize = "18px";
 text.style.fontStyle = "italic";
-text.style.color = "darkgreen";
+text.style.color = "black";
 
 const btn = document.getElementById("btn");
 
@@ -18,4 +18,18 @@ btn.addEventListener("click", () => {
     alert("button clicked");
     btn.style.backgroundColor= "#4CAF50";
     btn.style.color = "white";
+});
+
+const input = document.getElementById("input");
+const button = document.getElementById("showName");
+const output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+    if (input.value.trim() !== "") {
+        output.textContent = `hi, ${input.value}!`;
+        output.style.color = "green";
+    } else {
+        output.textContent = "please try again";
+        output.style.color = "red";
+    }
 });
